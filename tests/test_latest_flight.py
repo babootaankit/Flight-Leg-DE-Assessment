f01af75163e5b1b0daa757d1d6d5338cd52215cb
Babootaankit <34898171+babootaankit@users.noreply.github.com>
2025-08-19 13:20:31 -0400

    readme and docs updates, added yml file

--- tests/test_latest_flight.py
+++ tests/test_latest_flight.py
@@ -1,24 +1,29 @@
 # %%
-import pandas as pd
 import os
 import sys
 from unittest import mock
 
-import_path = str(os.path.abspath(os.path.join(os.path.dirname(__file__),"..")))
-sys.path.insert(0, import_path)
+import pandas as pd
+
 import latest_flight_function
+
+import_path = str(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, import_path)
+
 
 def test_get_latest_flight_dedup():
     # Dummy input simulating an Excel read
-    dummy_df = pd.DataFrame({
-        "flightkey": ["DL123", "DL123", "DL124"],
-        "flightnum": [3401, 3401, 31],
-        "flight_dt": ["2019-01-01", "2019-01-01", "2019-01-01"],
-        "orig_arpt": ["ATL", "ATL", "ATL"],
-        "dest_arpt": ["LAX", "LAX", "DFW"],
-        "flightstatus": ["Boarding", "In", "Boarding"],
-        "lastupdt": ["09:02:17", "09:20:17", "09:17:00"]
-    })
+    dummy_df = pd.DataFrame(
+        {
+            "flightkey": ["DL123", "DL123", "DL124"],
+            "flightnum": [3401, 3401, 31],
+            "flight_dt": ["2019-01-01", "2019-01-01", "2019-01-01"],
+            "orig_arpt": ["ATL", "ATL", "ATL"],
+            "dest_arpt": ["LAX", "LAX", "DFW"],
+            "flightstatus": ["Boarding", "In", "Boarding"],
+            "lastupdt": ["09:02:17", "09:20:17", "09:17:00"],
+        }
+    )
 
     # Patch pd.read_excel to return dummy_df instead of reading a file
     with mock.patch("pandas.read_excel", return_value=dummy_df):
